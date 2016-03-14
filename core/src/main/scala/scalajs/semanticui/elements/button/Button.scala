8e5b096f4aab3bde8bac2040c2e93e7f46b5d12c
Carlos Quiroz <cquiroz@gemini.edu>
2016-03-14 15:43:04 -0300

    Support tabindex for buttons

--- core/src/main/scala/scalajs/semanticui/elements/button/Button.scala
+++ core/src/main/scala/scalajs/semanticui/elements/button/Button.scala
@@ -3,20 +3,18 @@ package scalajs.semanticui.elements.button
 import japgolly.scalajs.react.{Callback, ReactComponentB, ReactNode}
 import japgolly.scalajs.react.vdom.prefix_<^._
 
-import scala.scalajs.js.UndefOr
-import scala.scalajs.js
-
 object Button {
   sealed trait ButtonState
   case object Active extends ButtonState
   case object Inactive extends ButtonState
 
-  case class Props(state: ButtonState, onClick: Callback = Callback.empty)
+  case class Props(state: ButtonState = Inactive, tabIndex: Option[Int] = None, onClick: Callback = Callback.empty)
 
   def component = ReactComponentB[Props]("Button")
     .renderPC((_, p, c) =>
       <.button(
         ^.cls := "ui button",
+        ^.tabIndex := p.tabIndex,
         ^.classSet(
           "active" -> (p.state == Active)
         ),
@@ -26,4 +24,5 @@ object Button {
     ).build
 
   def apply(p: Props, children: ReactNode*) = component(p, children: _*)
+  def apply(text: String) = component(Props(), text)
 }
--- examples/src/main/scala/scalajs/semanticui/elements/button/ButtonPage.scala
+++ examples/src/main/scala/scalajs/semanticui/elements/button/ButtonPage.scala
@@ -4,9 +4,8 @@ import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
 
 import scala.scalajs.js
-import scalajs.semanticui.elements.icon.{IconDefinitions, IconSets}
 
-object ButtonPage {
+object FollowExample {
   case class State(s: Button.ButtonState, t: String) {
     def text: String = s match {
       case Button.Inactive => "Follow"
@@ -25,24 +24,16 @@ object ButtonPage {
 
     def render(s: State) = {
       <.div(
-        ^.cls := "main ui container",
-        <.h2(
-          ^.cls := "ui dividing header",
-          "Types"
+        ^.cls := "follow example",
+        <.h4(
+          ^.cls := "ui header",
+          "Button"
         ),
-        <.div(
-          ^.cls := "follow example",
-          <.h4(
-            ^.cls := "ui header",
-            "Button"
-          ),
-          <.p("A standard button"),
-          Button(
-            Button.Props(
-              s.s,
-              onClick = click),
-            s.text
-          )
+        Button(
+          Button.Props(
+            s.s,
+            onClick = click),
+          s.text
         )
       )
     }
@@ -51,6 +42,59 @@ object ButtonPage {
   def component = ReactComponentB[Unit]("ButtonPage")
     .initialState(State(Button.Inactive, "Text"))
     .renderBackend[Backend]
+    .buildU
+
+  def apply() = component()
+}
+
+object AnotherExample {
+
+  def component = ReactComponentB[Unit]("ButtonPage")
+    .stateless
+    .render(_ =>
+      <.div(
+        ^.cls := "another example",
+        <.div(
+          ^.cls := "ui ignored warning message",
+          "Although any tag can be used for a button, it will only be ",
+          <.a(
+            ^.href := "http://webaim.org/techniques/keyboard/tabindex",
+            " keyboard focusable"),
+          "if you use a ",
+          <.code("<button>"),
+          "tag or you add the property ",
+          <.code("""tabindex="0""""),
+          ". Keyboard accessible buttons will preserve focus styles after click, which may be visually jarring."
+        ),
+        <.p("A standard button"),
+        Button(
+          "Button"
+        ),
+        Button(
+          Button.Props(tabIndex = Some(0)),
+          "Focusable"
+        )
+      )
+    )
+    .buildU
+
+  def apply() = component()
+}
+
+object ButtonPage {
+
+  def component = ReactComponentB[Unit]("ButtonPage")
+    .render(_ =>
+      <.div(
+        ^.cls := "main ui container",
+        <.h2(
+          ^.cls := "ui dividing header",
+          "Types"
+        ),
+        FollowExample(),
+        AnotherExample()
+      )
+    )
     .componentDidMount { _ => Callback {
         val semReady = js.Dynamic.global.semantic.ready
         //semReady()
