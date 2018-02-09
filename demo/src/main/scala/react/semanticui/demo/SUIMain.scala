db20b1ad861a6225605395935c5d34424c5d81dc
Carlos Quiroz <cquiroz@gemini.edu>
2018-02-09 13:20:16 -0300

    Measure the bundle sizes

---
+++ demo/src/main/scala/react/semanticui/demo/SUIMain.scala
@@ -0,0 +1,12 @@
+package react.semanticui.demo
+
+import scala.scalajs.js
+import js.annotation._
+
+@JSExportTopLevel("react.semanticui.demo.SUIMain")
+object SUiMain {
+  @JSExport
+  def main(): Unit = {
+    println("Main module")
+  }
+}
