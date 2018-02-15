6faebb20a89742c0a1801eafab2ba48930db36a6
Carlos Quiroz <cquiroz@gemini.edu>
2018-02-15 17:43:21 -0300

    Demo simplification

--- demo/src/main/scala/react/semanticui/demo/elements/IconsComponent.scala
+++ demo/src/main/scala/react/semanticui/demo/elements/IconsComponent.scala
@@ -10,6 +10,29 @@ import react.semanticui.colors
 object IconsComponent {
   private val sampleIcons = Map("mail outline" -> MailOutline, "search" -> Search)
 
+  def section(title: String, description: String)(icons: TagMod): TagMod =
+    <.div(
+      ^.cls := "equal width row",
+      <.div(
+        ^.cls := "ui padded one column grid",
+        <.div(
+          ^.cls := "column",
+          <.h2(
+            ^.cls := "ui header",
+            title
+          ),
+          <.p(description)
+        ),
+        <.div(
+          ^.cls := "center aligned column",
+          <.div(
+            ^.cls := "ui doubling five column grid",
+            icons
+          )
+        )
+      )
+    )
+
   private val component =
     ScalaComponent
       .builder[Unit]("Home")
@@ -30,137 +53,58 @@ object IconsComponent {
               )
             )
           ),
-          <.div(
-            ^.cls := "equal width row",
-            <.div(
-              ^.cls := "ui padded one column grid",
-              <.div(
-                ^.cls := "column",
-                <.h2(
-                  ^.cls := "ui header",
-                   "Icon Set"
-                ),
-                <.p("An icon set contains an arbitrary number of glyphs.")
-              ),
-              <.div(
-                ^.cls := "center aligned column",
+          section("Samples", "An icon set contains an arbitrary number of glyphs.") {
+            sampleIcons.map {
+              case (name, icon) =>
                 <.div(
-                  ^.cls := "ui doubling five column grid",
-                  <.div(
-                    ^.cls := "sixteen wide column",
-                    <.h3(
-                      ^.cls := "ui dividing left aligned header",
-                      "Samples"
-                    )
-                  ),
-                  sampleIcons.map {
-                    case (name, icon) =>
-                      <.div(
-                        ^.cls := "column docs-icon-set-column",
-                        icon.withSize(Big),
-                        <.p(
-                          ^.cls := "name",
-                          name
-                        )
-                      ),
-                  }.toTagMod
-                )
-              )
-            )
-          ),
-          <.div(
-            ^.cls := "equal width row",
-            <.div(
-              ^.cls := "ui padded one column grid",
-              <.div(
-                ^.cls := "column",
-                <.h2(
-                  ^.cls := "ui header",
-                   "Sizes"
+                  ^.cls := "column docs-icon-set-column",
+                  icon.withSize(Big),
+                  <.p(
+                    ^.cls := "name",
+                    name
+                  )
                 ),
-                <.p("An icon can vary in size")
-              ),
-              <.div(
-                ^.cls := "center aligned column",
+            }.toTagMod
+          },
+          section("Sizes", "An icon can vary in size") {
+            sizes.all.map {
+              case s =>
                 <.div(
-                  ^.cls := "ui doubling five column grid",
-                  sizes.all.map {
-                    case s =>
-                      <.div(
-                        ^.cls := "column docs-icon-set-column",
-                        Feed.withSize(s),
-                        <.p(
-                          ^.cls := "name",
-                          s.value
-                        )
-                      ),
-                  }.toTagMod
-                )
-              )
-            )
-          ),
-          <.div(
-            ^.cls := "equal width row",
-            <.div(
-              ^.cls := "ui padded one column grid",
-              <.div(
-                ^.cls := "column",
-                <.h2(
-                  ^.cls := "ui header",
-                   "Flipped"
+                  ^.cls := "column docs-icon-set-column",
+                  Feed.withSize(s),
+                  <.p(
+                    ^.cls := "name",
+                    s.value
+                  )
                 ),
-                <.p("An icon can be flipped")
-              ),
-              <.div(
-                ^.cls := "center aligned column",
+            }.toTagMod
+          },
+          section("Flipped", "An icon can be flipped") {
+            List(Horizontally, Vertically).map {
+              case o =>
                 <.div(
-                  ^.cls := "ui doubling five column grid",
-                  List(Horizontally, Vertically).map {
-                    case o =>
-                      <.div(
-                        ^.cls := "column docs-icon-set-column",
-                        CloudUpload.flipped(o),
-                        <.p(
-                          ^.cls := "name",
-                          o.value
-                        )
-                      ),
-                  }.toTagMod
-                )
-              )
-            )
-          ),
-          <.div(
-            ^.cls := "equal width row",
-            <.div(
-              ^.cls := "ui padded one column grid",
-              <.div(
-                ^.cls := "column",
-                <.h2(
-                  ^.cls := "ui header",
-                   "Colors"
+                  ^.cls := "column docs-icon-set-column",
+                  CloudUpload.flipped(o),
+                  <.p(
+                    ^.cls := "name",
+                    o.value
+                  )
                 ),
-                <.p("An icon can be flipped")
-              ),
-              <.div(
-                ^.cls := "center aligned column",
+            }.toTagMod
+          },
+          section("Colors", "Icons support colors") {
+            colors.all.map {
+              case o =>
                 <.div(
-                  ^.cls := "ui doubling five column grid",
-                  colors.all.map {
-                    case o =>
-                      <.div(
-                        ^.cls := "column docs-icon-set-column",
-                        Edit.withColor(o),
-                        <.p(
-                          ^.cls := "name",
-                          o.value
-                        )
-                      ),
-                  }.toTagMod
-                )
-              )
-            )
-          )
+                  ^.cls := "column docs-icon-set-column",
+                  Edit.withColor(o),
+                  <.p(
+                    ^.cls := "name",
+                    o.value
+                  )
+                ),
+            }.toTagMod
+          }
         )
       }
       .build
