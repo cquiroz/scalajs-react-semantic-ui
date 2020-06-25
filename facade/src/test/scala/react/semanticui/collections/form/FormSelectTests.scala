fef4031017dbf798515439936a125eb1c9b80929
Raúl Piaggio <rpiaggio@gmail.com>
2020-06-25 19:41:41 -0300

    FormSelect.

---
+++ facade/src/test/scala/react/semanticui/collections/form/FormSelectTests.scala
@@ -0,0 +1,23 @@
+package react.semanticui.collections.form
+
+import utest._
+import japgolly.scalajs.react.test._
+import react.semanticui.modules.dropdown.DropdownItem
+import react.semanticui.widths._
+
+object FormSelectTests extends TestSuite {
+  val tests = Tests {
+    test("options") {
+      val options =
+        List(DropdownItem("abc"), DropdownItem(text = "def", value = 2))
+      val select  = FormSelect(width = Two, options = options)
+      ReactTestUtils.withNewBodyElement { mountNode =>
+        select.renderIntoDOM(mountNode)
+        val html = mountNode.outerHTML
+        assert(
+          html == """<div><div class="two wide field"><div role="listbox" aria-expanded="false" class="ui selection dropdown" tabindex="0"><div class="text" role="alert" aria-live="polite" aria-atomic="true"></div><i aria-hidden="true" class="dropdown icon"></i><div class="menu transition"><div style="pointer-events: all;" role="option" aria-checked="false" aria-selected="true" class="selected item">abc</div><div style="pointer-events: all;" role="option" aria-checked="false" aria-selected="false" class="item"><span class="text">def</span></div></div></div></div></div>"""
+        )
+      }
+    }
+  }
+}
